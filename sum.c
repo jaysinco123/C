42399c2f97ceb9a91d59d9240eec371a413fd562
jaysinco123 <77875237+jaysinco123@users.noreply.github.com>
2021-01-23 19:59:14 +0800

    第一个c语言程序

---
+++ sum.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int n1 = 0;
+	int n2 = 0;
+	int sum = 0;
+	scanf("%d%D", &n1, &n2);
+	sum = n1 + n2;
+	printf("sum=%d\n", sum);
+	return 0;
+}
